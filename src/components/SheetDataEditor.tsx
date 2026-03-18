1b3f816bf27ec03ed4ab0a5fd86ab31241970808
roym <roymusafi@gmail.com>
2026-03-18 18:05:39 +0200

    fix

--- src/components/SheetDataEditor.tsx
+++ src/components/SheetDataEditor.tsx
@@ -273,7 +273,7 @@ const SheetDataEditor = ({ sheetData, onSaveProgress, onSaveToNewSheet,handleSav
         }
       const extractedDataArray = JSON.parse(data);
       let dataIndex = 0;
-      for (let rowIndex = headersRowIndex; rowIndex < dataRows.length - 2 && rowIndex < extractedDataArray.length; rowIndex++) {
+      for (let rowIndex = headersRowIndex; rowIndex < dataRows.length - 2; rowIndex++) {
           const extractedData = extractedDataArray[dataIndex++];
           for (let colIndex = 4; colIndex < minColIndex - 1; colIndex++) {
             const dataToInsert = getDataForHeader(colIndex, extractedData);
